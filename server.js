99978d305a8dcd68a724e33a4030f622d9958ec4
stuart-p <stuart_palmer85@hotmail.com>
2020-03-06 18:02:24 +0000

    server responds to connection events, and sends player list on connections. Sends player positions on client position updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-const arr = [];
+const players = {};
 
 http.listen(8080, () => {
   console.log("listening on 8080");
@@ -14,8 +14,25 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", socket => {
-  socket.on("player connected", data => {
-    arr.push(data);
-    io.emit(arr);
+  console.log("player connected");
+  players[socket.id] = { id: socket.id, pos: { x: 0, y: 0 } };
+  io.emit("playerArrayUpdate", players);
+
+  // socket.on("player connected", data => {
+  //   arr.push(data);
+  //   io.emit(arr);
+  // });
+  socket.on("playerMovement", data => {
+    // console.log("player: ", socket.id, "moving: ", data);
+  });
+  socket.on("playerPosUpdate", data => {
+    // console.log("player: ", socket.id, "moving: ", data);
+    players[socket.id].pos = data;
+    io.emit("playerArrayUpdate", players);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("socket");
+    delete players[socket.id];
   });
 });
