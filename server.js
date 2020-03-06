fcfb2f330ed2db3f942db92f922dce3d2271f3f8
jamie <jamiegormley1987@outlook.com>
2020-03-06 23:40:28 +0000

    can see movement of other players! from the server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 const arr = [];
+let playerNumber = 1;
 
 http.listen(8080, () => {
   console.log("listening on 8080");
@@ -14,8 +15,24 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", socket => {
+  socket.on("playerJoined", msg => {
+    socket.emit("connection verified", playerNumber);
+    playerNumber++;
+  });
+
   socket.on("player connected", data => {
     arr.push(data);
-    io.emit(arr);
+    io.emit("player connected update", arr);
+    console.log("playerconupdate");
+  });
+  socket.on("playerMovement", data => {
+    console.log("someone said they moved");
+    arr.forEach(player => {
+      if (player.id === data.id) {
+        player.x = data.x;
+        player.y = data.y;
+      }
+      io.emit("player connected update", arr);
+    });
   });
 });
